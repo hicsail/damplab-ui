b65867fe2f4b908f62aca4dcf108fd9f5e0a680e
chris-krenz <ckrenz@bu.edu>
2024-07-06 16:09:16 -0400

    started job submission page updates

--- src/pages/JobSubmitted.tsx
+++ src/pages/JobSubmitted.tsx
@@ -1,5 +1,10 @@
 import React, { useState }    from 'react'
 import        { useLocation } from 'react-router-dom';
+import { useQuery }           from '@apollo/client';
+import { GET_JOB_BY_ID }      from '../gql/queries';
+
+import { Workflow } from '../gql/graphql';
+
 // import QRCode from "react-qr-code";
 
 
@@ -11,6 +16,35 @@ export default function JobSubmitted() {
   const [jobId, setJobId] = useState(location.state.id);
   const [value, setValue] = useState(`https://damplab.sail.codes/client_view/${jobId}`);
 
+  const [workflowName, setWorkflowName]               = useState('');
+  const [workflowState, setWorkflowState]             = useState('');
+  const [jobName, setJobName]                         = useState('');
+  const [jobState, setJobState]                       = useState('');
+  const [jobTime, setJobTime]                         = useState('');
+  const [workflowUsername, setWorkflowUsername]       = useState('');
+  const [workflowInstitution, setWorkflowInstitution] = useState('');
+  const [workflowEmail, setWorkflowEmail]             = useState('');
+  const [workflows, setWorklows]                      = useState<Workflow[]>([]);
+
+  const { loading, error, data } = useQuery(GET_JOB_BY_ID, {
+    variables: { id: jobId },
+    onCompleted: (data) => {
+        console.log('job successfully loaded: ', data);
+        setWorkflowName(data.jobById.workflows[0].name);
+        setWorkflowState(data.jobById.workflows[0].state);
+        setJobName(data.jobById.name);
+        setJobState(data.jobById.state);
+        setJobTime(data.jobById.submitted);
+        setWorkflowUsername(data.jobById.username);
+        setWorkflowInstitution(data.jobById.institute);
+        setWorkflowEmail(data.jobById.email);
+        setWorklows(data.jobById.workflows);
+    },
+    onError: (error: any) => {
+        console.log(error.networkError?.result?.errors);
+    }
+});
+
   return (
 
     <div>
@@ -21,6 +55,10 @@ export default function JobSubmitted() {
 
       <p>
         Job ID: {jobId}
+      </p>
+
+      <p>
+        {workflows[0].name}
       </p>
 
       {/* Functional but disabled for now... */}
