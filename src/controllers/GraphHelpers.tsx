8ea4d053290068e7881dce4bcedd628e101e5bd0
Asad <am5815@bu.edu>
2024-08-19 13:14:46 -0400

    saved correctly for previous commit

--- src/controllers/GraphHelpers.tsx
+++ src/controllers/GraphHelpers.tsx
@@ -173,7 +173,6 @@ export const addNodesAndEdgesFromServiceIds = (services: any[], serviceIds: stri
     let baseY = 0;
     
     serviceIds?.forEach((serviceId: string, index: number) => {
-        console.log('serviceId: ', serviceId);
         // get service from serviceId
         const service = getServiceFromId(services, serviceId);
         // if index === 0, add node to canvas with edge
@@ -195,13 +194,11 @@ export const addNodesAndEdgesFromServiceIds = (services: any[], serviceIds: stri
 export const addNodesAndEdgesFromBundle = (bundle: any, services: any, setNodes: any, setEdges: any) => {
     // get serviceIds from bundle
     const bundleServices = bundleServiceOrders.find(b => b.label == bundle.label)?.services;
-    console.log('bundleServices: ', bundleServices);
+    
     const bundleServiceNames = bundleServices?.map(service => serviceNames.find(s => s.id === service)?.name);
     const serviceIds = bundleServiceNames?.map(service => bundle.services.find((s: any) => s.name === service)?.id ?? '');
     // console.log('test: ', test);
     // const serviceIds = bundle.services.map((service: any) => service.id);
-    console.log('serviceIds: ', serviceIds);
-    console.log('services: ', services);
     addNodesAndEdgesFromServiceIds(services, serviceIds, setNodes, setEdges);
 }
 
@@ -220,7 +217,7 @@ export const paramsFilledOnNode = (node: any) : Boolean => {
 export const searchForEndService : any = (serviceId : string, endServiceId: string, visited: any[]) => {
     
     const service: any | undefined = services.find(s => s.id === serviceId);
-    console.log(service);
+   
     if (!service) {
         return null;
     }
@@ -228,7 +225,7 @@ export const searchForEndService : any = (serviceId : string, endServiceId: stri
         return null;
     }
     visited.push(service);
-    console.log(visited);
+  
     if (service.id === endServiceId) {
       return service;
     }
@@ -269,7 +266,7 @@ export const transformGQLforDominos = (workflow: any) => {
 };
 
 export const transformGQLToWorkflow = (workflow: any) => {
-    console.log(workflow);
+    
     let nodes = workflow.nodes.map((node: any) => {
         return {
             id         : node._id,
