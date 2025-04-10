8c68c8f556c62638c71bddda5655f3fe54ac1e67
Zoe <19900057+vpsx@users.noreply.github.com>
2025-04-10 13:31:41 -0400

    Upgrade to React 19, use new usercontext in route guard components

--- src/components/PrivateRouteClient.tsx
+++ src/components/PrivateRouteClient.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
+import React, { use, useContext } from "react";
+import { Navigate } from "react-router-dom";
+import {
+  UserContext,
+  UserContextProps,
+  UserProps,
+} from "../contexts/UserContext";
 
 // Clients can only access certain pages, such as the Canvas, Checkout, and Submission Confirmation
 const PrivateRouteClient = ({ children }: any) => {
+  const userContext: UserContextProps = useContext(UserContext);
+  const userProps: UserProps = use(userContext.userProps);
 
-  const loginInfo = JSON.parse(sessionStorage.getItem('login_info') || '{}');
-  const isAuthenticated = loginInfo && loginInfo.loggedIn;
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-
+  return userProps?.isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRouteClient;
