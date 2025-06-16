84b5d55defff0944bd1e3bbc7d276318da180f73
Zoe <19900057+vpsx@users.noreply.github.com>
2025-06-16 12:12:35 -0400

    Fix abominable css on landing pg menu buttons

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -10,6 +10,18 @@ import EditIcon from '@mui/icons-material/Edit';
 import { UserContext, UserContextProps, UserProps } from "../contexts/UserContext";
 
 
+function MenuButton({onClick, navigateTo, children}) {
+    const navigate = useNavigate();
+    return (
+        <Button variant="contained" onClick={onClick ? onClick : () => navigate(navigateTo)}  sx={{ m: 2, width: '210px', textTransform: 'none' }}>
+            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                {children[0]}
+                <Box sx={{width: '100%'}}>{children.slice(1)}</Box>
+            </Box>
+        </Button>
+    );
+}
+
 export default function LoginForm() {
   const navigate = useNavigate();
   const userContext : UserContextProps = useContext(UserContext);
@@ -33,32 +45,19 @@ export default function LoginForm() {
         <>
           <Box sx={{ width: '300px' }}>
             <p>Hello, {appellation}.</p>
-            <Stack direction="row" spacing={1}>
+            <Stack direction="row" spacing={1} sx={{marginBottom: 5}}>
               {userProps.isDamplabStaff && <Chip label="DAMPLab Staff" />}
               {userProps.isInternalCustomer && <Chip label="Internal Customer" />}
               {userProps.isExternalCustomer && <Chip label="External Customer" />}
             </Stack>
-            {/* <Button variant="contained" onClick={() => navigate(role === 'admin' ? '/dashboard' : '/canvas')} style={{ marginRight: 10 }}>
-              Go to {role === 'admin' ? 'Dashboard' : 'Canvas'}
-            </Button> */}
-            <Button variant="contained" onClick={() => window.location.href = "https://www.damplab.org/services"} sx={{ m: 2, width: '210px', textTransform: 'none' }}>
-              <img src='/damp-white.svg' height='30px' style={{margin: 1, marginLeft: -25, marginRight: 10}} alt="DAMP Logo"/>DAMPLab Site<br/>(See Service Prices)
-            </Button>
-            <Button variant="contained" onClick={() => navigate('/canvas')}  sx={{ m: 2, width: '210px', textTransform: 'none' }}>
-              <AccountTreeIcon sx={{m:1, ml:-4, transform: "rotate(90deg) scaleY(-1)"}}/>CANVAS<br/>(Design Workflows)
-            </Button>
+            <MenuButton onClick={() => window.location.href = "https://www.damplab.org/services"}><img src='/damp-white.svg' height='30px' alt="DAMP Logo"/>DAMPLab Site<br/>(See Service Prices)</MenuButton>
+            <MenuButton navigateTo='/canvas'><AccountTreeIcon sx={{transform: "rotate(90deg) scaleY(-1)"}}/>Canvas<br />(Design Workflows)</MenuButton>
             {userProps.isDamplabStaff ?
               <>
-                <Button variant="contained" onClick={() => navigate('/dashboard')}  sx={{ m: 2, width: '210px', textTransform: 'none' }}>
-                  <ViewStreamIcon sx={{m:1, ml:-3}}/>DASHBOARD<br/>(See Submitted Jobs)
-                </Button>
-                <Button variant="contained" onClick={() => navigate('/edit')}  sx={{ m: 2, width: '210px', textTransform: 'none' }}>
-                  <EditIcon sx={{m:1, ml:-8}} />Admin Edit<br/>(Edit Services)
-                </Button>
-                <Button variant="contained" onClick={() => navigate('/release_notes')}  sx={{ m: 2, width: '210px', textTransform: 'none' }}>
-                  <FormatListBulletedIcon sx={{m:1, ml:-3}}/>Release Notes<br/>(+ Other Admin Info)
-                </Button> 
-                {/* <Button variant="contained" onClick={() => navigate('/dominos')} sx={{ m: 2 }}>Go to Dominos Page</Button> 
+                <MenuButton navigateTo='/dashboard'><ViewStreamIcon />Dashboard<br />(See Submitted Jobs)</MenuButton>
+                <MenuButton navigateTo='/edit'><EditIcon />Admin Edit<br />(Edit Services)</MenuButton>
+                <MenuButton navigateTo='/release_notes'><FormatListBulletedIcon />Release Notes<br />(+ Other Admin Info)</MenuButton>
+                {/* <Button variant="contained" onClick={() => navigate('/dominos')} sx={{ m: 2 }}>Go to Dominos Page</Button>
                 <Button variant="contained" onClick={() => navigate('/elabs')} sx={{ m: 2 }}>Go to eLabs Site</Button> 
                 <Button variant="contained" onClick={() => navigate('/kernel')} sx={{ m: 2 }}>Go to Kernel Site</Button>  */}
               </>
