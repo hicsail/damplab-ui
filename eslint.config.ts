e002a8af053e611e9d5f2d595b7a23bf5c6c8475
TheAlden <alden@jerrycarter.org>
2025-08-18 13:49:01 -0400

    chore: change eslint.config to .ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,27 +1,21 @@
 import { defineConfig } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import prettierPlugin from "eslint-plugin-prettier";
-import tsParser from "@typescript-eslint/parser";
 
 export default defineConfig({
   files: ["src/**/*.{ts,tsx}"],
-  ignores: ["src/gql/**"], 
+  ignores: ["src/gql/**"],
   plugins: {
     "@typescript-eslint": tsPlugin,
     prettier: prettierPlugin,
   },
-  /*extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],*/
   rules: {
-    "prettier/prettier": ["warn", {}, { "usePrettierrc": true }],
-    "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
   },
   languageOptions: {
     parser: tsParser,
@@ -40,6 +34,6 @@ export default defineConfig({
       expect: "readonly",
       beforeEach: "readonly",
       afterEach: "readonly",
-    }
-  }
+    },
+  },
 });
