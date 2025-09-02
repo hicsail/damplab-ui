a8fbbab89f07076bee94974f1ab63d09689f79c7
Asad <am5815@bu.edu>
2025-09-02 13:14:42 -0400

    merge resolve

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate, Navigate } from 'react-router';
 import { useApolloClient } from '@apollo/client';
 import { Box, Button, Chip, Stack } from '@mui/material';
 
@@ -27,18 +27,23 @@ function MenuButton({onClick, navigateTo, children} : any) {
 export default function LoginForm() {
   const apolloClient = useApolloClient();
   const navigate = useNavigate();
+  // Technically we should clear the Apollo client cache on logout, but right now, even without the resetStore() 
+  // call, all queries get refetched anyway because the keycloak logout redirects essentially trigger a reload.
+  // So this is redundant by happenstance. Do it anyway to be proper...
   const userContext: UserContextProps = useContext(UserContext);
-  const userProps : UserProps = userContext.userProps;
+  const userProps = userContext.userProps;
 
   function logout() {
-      // Technically we should clear the Apollo client cache on logout, but right now, even without the resetStore()
-      // call, all queries get refetched anyway because the keycloak logout redirects essentially trigger a reload.
-      // So this is redundant by happenstance. Do it anyway to be proper...
       apolloClient.resetStore();
       userContext.keycloak.logout();
   }
 
-  const appellation = userProps?.idTokenParsed?.name || userProps?.idTokenParsed?.email || "DAMPLab User";
+  const appellation = (userProps?.idTokenParsed as any)?.name || (userProps?.idTokenParsed as any)?.email || "DAMPLab User";
+
+  if (userProps?.isAuthenticated) {
+    // Redirect to protected home page
+    return <Navigate to="/" replace />;
+  }
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
@@ -48,58 +53,53 @@ export default function LoginForm() {
         </Button>
       ) : (
         <>
-<Box sx={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'center' }}>
+          <Box sx={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'center' }}>
 
-  <Box sx={{ mb: 3, textAlign: 'center' }}>
-    <p>Hello, {appellation}.</p>
-    <Stack direction="row" spacing={1} sx={{ marginBottom: 5, justifyContent: 'center' }}>
-      {userProps.isDamplabStaff && <Chip label="DAMPLab Staff" />}
-      {userProps.isInternalCustomer && <Chip label="Internal Customer" />}
-      {userProps.isExternalCustomer && <Chip label="External Customer" />}
-    </Stack>
-    {/* <Button variant="contained" onClick={() => navigate(role === 'admin' ? '/dashboard' : '/canvas')} style={{ marginRight: 10 }}> 
-      Go to {role === 'admin' ? 'Dashboard' : 'Canvas'} </Button> */}
-  </Box>
+            <Box sx={{ mb: 3, textAlign: 'center' }}>
+              <p>Hello, {appellation}.</p>
+              <Stack direction="row" spacing={1} sx={{ marginBottom: 5, justifyContent: 'center' }}>
+                {userProps.isDamplabStaff && <Chip label="DAMPLab Staff" />}
+                {userProps.isInternalCustomer && <Chip label="Internal Customer" />}
+                {userProps.isExternalCustomer && <Chip label="External Customer" />}
+              </Stack>
+            </Box>
 
+            {/* Left Column: user buttons  - annoucement box side to side */}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                width: '100%',
+              }}
+            >
+              {/* User options buttons */}
+              <Box sx={{ width: '300px' }}>
+                <MenuButton onClick={() => window.location.href = "https://www.damplab.org/services"}><img src='/damp-white.svg' height='30px' alt="DAMP Logo"/>DAMPLab Site<br/>(See Service Prices)</MenuButton>
 
-  {/* Left Column: user buttons  - annoucement box side to side */}
+                <MenuButton navigateTo='/canvas'><AccountTreeIcon sx={{transform: "rotate(90deg) scaleY(-1)"}}/>Canvas<br />(Design Workflows)</MenuButton>
 
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'flex-start',
-      justifyContent: 'center',
-      width: '100%',
-    }}
-  >
-    {/* User options buttons */}
-    <Box sx={{ width: '300px' }}>
-      <MenuButton onClick={() => window.location.href = "https://www.damplab.org/services"}><img src='/damp-white.svg' height='30px' alt="DAMP Logo"/>DAMPLab Site<br/>(See Service Prices)</MenuButton>
+                {userProps.isDamplabStaff && (
+                  <>
+                    <MenuButton navigateTo='/dashboard'><ViewStreamIcon />Dashboard<br />(See Submitted Jobs)</MenuButton>
+                    <MenuButton navigateTo='/edit'><EditIcon />Admin Edit<br />(Edit Services)</MenuButton>
+                    <MenuButton navigateTo='/release_notes'><FormatListBulletedIcon />Release Notes<br />(+ Other Admin Info)</MenuButton>
+                    <MenuButton navigateTo='/data_translation'><EditIcon />Data Translation<br />(Abbott to eLabs formatting)</MenuButton>
+                    <MenuButton navigateTo='/edit_announcements'><CampaignIcon />Add Announcement<br />(+ Edit)</MenuButton>
+                  </>
+                )}
+                <Button variant="contained" color="secondary" onClick={logout}  sx={{ m: 5 }}>Logout</Button>
+              </Box>
 
-      <MenuButton navigateTo='/canvas'><AccountTreeIcon sx={{transform: "rotate(90deg) scaleY(-1)"}}/>Canvas<br />(Design Workflows)</MenuButton>
+              {/* Right Column: AnnouncementBox (conditionally shown) */}
+              <AnnouncementBox />
+        
+            </Box>
 
-      {userProps.isDamplabStaff && (
-        <>
-          <MenuButton navigateTo='/dashboard'><ViewStreamIcon />Dashboard<br />(See Submitted Jobs)</MenuButton>
-          <MenuButton navigateTo='/edit'><EditIcon />Admin Edit<br />(Edit Services)</MenuButton>
-          <MenuButton navigateTo='/release_notes'><FormatListBulletedIcon />Release Notes<br />(+ Other Admin Info)</MenuButton>
-          <MenuButton navigateTo='/data_translation'><EditIcon />Data Translation<br />(Abbott to eLabs formatting)</MenuButton>
-          <MenuButton navigateTo='/edit_announcements'><CampaignIcon />Add Announcement<br />(+ Edit)</MenuButton>
+          </Box>
         </>
       )}
-      <Button variant="contained" color="secondary" onClick={logout}  sx={{ m: 5 }}>Logout</Button>
     </Box>
-
-        {/* Right Column: AnnouncementBox (conditionally shown) */}
-          <AnnouncementBox />
-  
-    </Box>
-
-  </Box>
-
-  </>
-    )}
-</Box>
   );
 }
