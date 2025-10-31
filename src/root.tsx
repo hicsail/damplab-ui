116a1e4c7550fbeca833f3fea210dd373371affc
Zoe <19900057+vpsx@users.noreply.github.com>
2025-10-31 14:30:03 -0400

    Rm manifest.json, public/other/favicon.png
    * this is not a PWA
    * manifest.json referenced long-dead assets, caused error msgs, clutter

--- src/root.tsx
+++ src/root.tsx
@@ -32,11 +32,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/favicon.ico?v=5" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        {/*
-                manifest.json provides metadata used when your web app is installed on a
-                user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-        */}
-        <link rel="manifest" href="/manifest.json" />
         <title>DAMPLab Canvas</title>
         <Meta />
         <Links />
